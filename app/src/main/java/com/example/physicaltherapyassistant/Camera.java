9619e874cb72b30608726080bfb22a7286a5f471
Justin Yue <flyingjay16@gmail.com>
2019-07-02 00:12:28 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/physicaltherapyassistant/Camera.java
@@ -0,0 +1,173 @@
+package com.example.physicaltherapyassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Camera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    private static final String TAG = "MainActivity";
+
+    private CameraBridgeViewBase tracker;
+
+    private Mat inputFrame;
+    private Mat mat1;
+    private Mat mat2;
+
+    private Scalar lScalar = new Scalar(100,112,50);  //100,112,50        112,131,15
+    private Scalar uScalar = new Scalar(180,255,255); //180,255,255       180,255,255
+
+    private Mat hsvMatI;
+    private Mat hsvMatO;
+
+    private Mat mBlurMat;
+
+    private Mat hierarchy;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+
+            switch(status) {
+                case BaseLoaderCallback.SUCCESS:
+                    tracker.enableView();
+                    inputFrame = new Mat();
+                    mat1 = new Mat();
+                    mat2 = new Mat();
+                    hsvMatI = new Mat();
+                    hsvMatO = new Mat();
+                    mBlurMat = new Mat();
+                    hierarchy = new Mat();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: called");
+
+        tracker = findViewById(R.id.tracker);
+        tracker.setVisibility(SurfaceView.VISIBLE);
+        tracker.setCameraIndex(0);
+        tracker.setCvCameraViewListener(this);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume: called");
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause: called");
+
+        if(tracker != null) {
+            tracker.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy: called");
+
+        if(tracker != null) {
+            tracker.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        inputFrame = new Mat(width, height, CvType.CV_16UC4);
+        mat1 = new Mat(width, height, CvType.CV_16UC4);
+        mat2 = new Mat(width, height, CvType.CV_16UC4);
+        hsvMatI = new Mat(width, height, CvType.CV_16UC4);
+        hsvMatO = new Mat(width, height, CvType.CV_16UC4);
+        mBlurMat = new Mat(width, height, CvType.CV_16UC4);
+        hierarchy = new Mat(width, height, CvType.CV_16UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        inputFrame.release();
+        mat1.release();
+        mat2.release();
+
+        hsvMatI.release();
+        hsvMatO.release();
+
+        mBlurMat.release();
+
+        hierarchy.release();
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame input) {
+        inputFrame = input.rgba();
+
+        Core.transpose(inputFrame, mat1);
+        Imgproc.resize(mat1, mat2, inputFrame.size(), 0, 0, 0);
+        Core.flip(mat2, inputFrame, 1);
+
+        Imgproc.cvtColor(inputFrame, hsvMatI, Imgproc.COLOR_RGB2HSV_FULL);
+        Core.inRange(hsvMatI, lScalar, uScalar, hsvMatO);
+
+        Imgproc.medianBlur(hsvMatO, mBlurMat, 35); //45,55
+
+        List<MatOfPoint> contourList = new ArrayList<MatOfPoint>();
+
+        Imgproc.findContours(mBlurMat, contourList, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        for(int i = 0 ; i < contourList.size(); i++) {
+            hierarchy.release();
+            Imgproc.drawContours(inputFrame, contourList, i,  new Scalar(255,0,0), 2);
+            //contour gets smaller when bent
+            //could change b/c of body type???
+        }
+
+        return inputFrame;
+    }
+}
---
+++ app/src/main/java/com/example/physicaltherapyassistant/Lift.java
@@ -0,0 +1,75 @@
+package com.example.physicaltherapyassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class Lift extends AppCompatActivity {
+
+    Accelerometer accelerometer;
+    Gyroscope gyroscope;
+    TextView xText, yText, zText;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lift);
+
+        accelerometer = new Accelerometer(this);
+        gyroscope = new Gyroscope(this);
+        handler = new Handler();
+        xText = findViewById(R.id.A_X);
+        yText = findViewById(R.id.A_Y);
+        zText = findViewById(R.id.A_Z);
+
+        accelerometer.setListener(new Accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+                xText.setText("Delta X: " + tx);
+                yText.setText("Delta Y: " + ty);
+                zText.setText("Delta Z: " + tz);
+                if(ty > -0.4 && ty < 0.4) {
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                }
+                else {
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }
+            }
+        });
+
+
+        /*gyroscope.setListener(new Gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+                if(ry >= 2) {
+                    getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                        }
+                    }, 1000);
+
+                }
+            }
+        });*/
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        accelerometer.register();
+        gyroscope.register();
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        accelerometer.unRegister();
+        gyroscope.unregister();
+    }
+}
